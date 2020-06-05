766571387c69ce16eec10ca5cd9ed15c21a17192
1104486368 <1104486368@qq.com>
2020-06-05 12:12:29 +0800

    自定义列表（运行出错？需要再改）

--- app/src/main/java/com/swufe/best/MyList2Activity.java
+++ app/src/main/java/com/swufe/best/MyList2Activity.java
@@ -3,12 +3,24 @@ package com.swufe.best;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.SimpleAdapter;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class MyList2Activity extends ListActivity {
+public class MyList2Activity extends ListActivity implements AdapterView.OnItemClickListener {
     Handler handler;
     // 存放文字、图片信息
     private ArrayList<HashMap<String, String>> listItems;
@@ -19,7 +31,71 @@ public class MyList2Activity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initListView();
+        this.setListAdapter(listItemAdapter);
+
+
+        Thread t = new Thread((Runnable) this); // 创建新线程
+        t.start(); // 开启线程
+
         }
+
+
+
+
+
+    public void run() {
+        Log.i("thread","run.....");
+        boolean marker = false;
+        List<HashMap<String, String>> rateList = new ArrayList<HashMap<String, String>>();
+
+        try {
+            Document doc = Jsoup.connect("http://www.usd-cny.com/icbc.htm").get();
+            Elements tbs = doc.getElementsByClass("tableDataTable");
+            Element table = tbs.get(0);
+            Elements tds = table.getElementsByTag("td");
+            for (int i = 6; i < tds.size(); i+=6) {
+                Element td = tds.get(i);
+                Element td2 = tds.get(i+3);
+                String tdStr = td.text();
+                String pStr = td2.text();
+
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put("ItemTitle", tdStr);
+                map.put("ItemDetail", pStr);
+
+                rateList.add(map);
+                Log.i("td",tdStr + "=>" + pStr);
+            }
+            marker = true;
+        } catch (MalformedURLException e) {
+            Log.e("www", e.toString());
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("www", e.toString());
+            e.printStackTrace();
+        }
+
+        Message msg = handler.obtainMessage();
+        msg.what = msgWhat;
+        if(marker){
+            msg.arg1 = 1;
+        }else{
+            msg.arg1 = 0;
+        }
+
+        msg.obj = rateList;
+        handler.sendMessage(msg);
+
+        Log.i("thread","sendMessage.....");
+
+
+
+    }
+
+
+
+
 
 
         private void initListView() {
@@ -41,6 +117,17 @@ public class MyList2Activity extends ListActivity {
                     new int[] { R.id.itemTitle, R.id.itemDetail }
             );
 
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
 }
